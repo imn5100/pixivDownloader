12cb190fd4022cc4b4cd16db5a664a15e66a6f7d
tuanzi <tuanzi@2dfire.com>
2016-12-10 17:27:28 +0800

    update

--- launcher_plus.py
+++ launcher_plus.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     # Pixivision全站插图爬取
     # 目前有62页 共61*20+ 5 特辑（2016/12/10 2:44数据）
     urls = [LINK_URL % n for n in range(1, 63)]
-    # 步伐，每次启动 4 *20 个进程
-    step = 4
+    # 步伐，每次启动 2 *20 个进程
+    step = 2
     length = len(urls)
     start = 0
     while start < length:
         launchers = []
         for url in urls[start:(start + step)]:
-            print("start:" + url)
+            print("Start " + url)
             launchers.append(PixivisionLauncher(url, "E://imageDownLoad//z_pixivision_download", 1))
         for launcher in launchers:
             launcher.start()
