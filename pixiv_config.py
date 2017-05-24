a405b7c5dcde7927de1056f04d662efa7c81ec58
xiaowc <xiaowc@dxy.cn>
2017-05-24 15:46:44 +0800

    update config

--- pixiv_config.py
+++ pixiv_config.py
@@ -36,9 +36,9 @@ BASE_URL = "http://www.pixivision.net"
 # 拉取插画专题 url
 LINK_URL = "http://www.pixivision.net/en/c/illustration/?p=%s"
 ILLUST_DETAIL_PAGE = "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=%s"
-# Pixivision全站总页数,用于全站爬取 2017/5/10
+# Pixivision全站总页数,用于全站爬取 2017/5/24
 # 全站爬取完毕后，如果Pixivsion有更新，可以修改此配置，比如Pixivsion有2页更新未爬取，修改PAGE_NUM=2,全站插画爬虫则会爬取前2页的所有特辑
-PAGE_NUM = config.getint("PAGE_NUM", default_value=76)
+PAGE_NUM = config.getint("PAGE_NUM", default_value=77)
 # illust detail url
 ILLUST_DETAIL = "https://app-api.pixiv.net/v1/illust/detail"
 ILLUST_RELATED = 'https://app-api.pixiv.net/v1/illust/related'
