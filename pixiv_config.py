ac68b6aff217aaa29ecbf93da8de87ce7e62620f
shaw <imn5100_chu2@163.com>
2017-09-10 11:53:43 +0800

    update pixiv config

--- pixiv_config.py
+++ pixiv_config.py
@@ -60,7 +60,7 @@ ILLUST_RELATED = 'https://app-api.pixiv.net/v2/illust/related'
 # 是否覆盖已下载的插画,False 时，已下载的插画会跳过下载，True时，无论插画是否存在，都会下载，并覆盖原文件
 OVERRIDE_IMAGE = config.getboolean("OVERRIDE_IMAGE", default_value=False)
 # 存储插画的基本目录
-IMAGE_SAVE_BASEPATH = config.getint("IMAGE_SAVE_BASEPATH",
+IMAGE_SAVE_BASEPATH = config.get("IMAGE_SAVE_BASEPATH",
                                     default_value=' /Users/imn5100/Downloads/pixiv/z_pixivision_download')
 # 文件命名是否使用原文件名（即插画作者的命名）
 # 因为插画原名经常出现颜文字和各种奇怪的符号，这里不使用图片标题进行命名，用pixiv 的id进行命名会很大地减少文件错误，提高下载正确率，
